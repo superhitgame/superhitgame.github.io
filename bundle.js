9050d8d6607315e6cd6cea8e87ac0afebe28a9ff
robrecht <robrechth@gmail.com>
2016-01-29 19:48:19 +0100

    fixed input

--- bundle.js
+++ bundle.js
@@ -584,10 +584,11 @@
 	  self.canvas = canvas;
 	  self.drawingManager = drawingManager;
 	  if ("ontouchstart" in window) {
-	    //log("init touch");
+	    //log("init touch"); 
 	    self.canvas.addEventListener("touchstart", function(e) {
 	      e.preventDefault();
-	      self.drawingManager.startPen(e.touches[0].pageX - self.canvas.offsetLeft, e.touches[0].pageY - self.canvas.offsetTop);
+	      var outer = self.canvas.getBoundingClientRect();
+	      self.drawingManager.startPen(e.touches[0].pageX - outer.left, e.touches[0].pageY - outer.top);
 	    }, false);
 	    self.canvas.addEventListener("touchend", function(e) {
 	      e.preventDefault();
@@ -595,21 +596,25 @@
 	    }, false);
 	    self.canvas.addEventListener("touchmove", function(e) {
 	      e.preventDefault();
-	      self.drawingManager.movePen(e.touches[0].pageX - self.canvas.offsetLeft, e.touches[0].pageY - self.canvas.offsetTop);
+	      var outer = self.canvas.getBoundingClientRect();
+	      self.drawingManager.movePen(e.touches[0].pageX - outer.left, e.touches[0].pageY - outer.top);
 	    }, false);
 	  } else {
 	    //log("init mouse");
 	    self.canvas.addEventListener("mousedown", function(e) {
 	      e.preventDefault();
-	      self.drawingManager.startPen(e.pageX - self.canvas.offsetLeft, e.pageY - self.canvas.offsetTop);
+	      var outer = self.canvas.getBoundingClientRect();
+	      self.drawingManager.startPen(e.pageX - outer.left, e.pageY - outer.top);
 	    }, false);
 	    self.canvas.addEventListener("mouseup", function(e) {
 	      e.preventDefault();
-	      self.drawingManager.stopPen(e.pageX - self.canvas.offsetLeft, e.pageY - self.canvas.offsetTop);
+	      var outer = self.canvas.getBoundingClientRect();
+	      self.drawingManager.stopPen(e.pageX - outer.left, e.pageY - outer.top);
 	    }, false);
 	    self.canvas.addEventListener("mousemove", function(e) {
 	      e.preventDefault();
-	      self.drawingManager.movePen(e.pageX - self.canvas.offsetLeft, e.pageY - self.canvas.offsetTop);
+	      var outer = self.canvas.getBoundingClientRect();
+	      self.drawingManager.movePen(e.pageX - outer.left, e.pageY - outer.top);
 	    }, false);
 	  }
 	}
--- inputArea.js
+++ inputArea.js
@@ -5,10 +5,11 @@ function InputArea(canvas, drawingManager) {
   self.canvas = canvas;
   self.drawingManager = drawingManager;
   if ("ontouchstart" in window) {
-    //log("init touch");
+    //log("init touch"); 
     self.canvas.addEventListener("touchstart", function(e) {
       e.preventDefault();
-      self.drawingManager.startPen(e.touches[0].pageX - self.canvas.offsetLeft, e.touches[0].pageY - self.canvas.offsetTop);
+      var outer = self.canvas.getBoundingClientRect();
+      self.drawingManager.startPen(e.touches[0].pageX - outer.left, e.touches[0].pageY - outer.top);
     }, false);
     self.canvas.addEventListener("touchend", function(e) {
       e.preventDefault();
@@ -16,21 +17,25 @@ function InputArea(canvas, drawingManager) {
     }, false);
     self.canvas.addEventListener("touchmove", function(e) {
       e.preventDefault();
-      self.drawingManager.movePen(e.touches[0].pageX - self.canvas.offsetLeft, e.touches[0].pageY - self.canvas.offsetTop);
+      var outer = self.canvas.getBoundingClientRect();
+      self.drawingManager.movePen(e.touches[0].pageX - outer.left, e.touches[0].pageY - outer.top);
     }, false);
   } else {
     //log("init mouse");
     self.canvas.addEventListener("mousedown", function(e) {
       e.preventDefault();
-      self.drawingManager.startPen(e.pageX - self.canvas.offsetLeft, e.pageY - self.canvas.offsetTop);
+      var outer = self.canvas.getBoundingClientRect();
+      self.drawingManager.startPen(e.pageX - outer.left, e.pageY - outer.top);
     }, false);
     self.canvas.addEventListener("mouseup", function(e) {
       e.preventDefault();
-      self.drawingManager.stopPen(e.pageX - self.canvas.offsetLeft, e.pageY - self.canvas.offsetTop);
+      var outer = self.canvas.getBoundingClientRect();
+      self.drawingManager.stopPen(e.pageX - outer.left, e.pageY - outer.top);
     }, false);
     self.canvas.addEventListener("mousemove", function(e) {
       e.preventDefault();
-      self.drawingManager.movePen(e.pageX - self.canvas.offsetLeft, e.pageY - self.canvas.offsetTop);
+      var outer = self.canvas.getBoundingClientRect();
+      self.drawingManager.movePen(e.pageX - outer.left, e.pageY - outer.top);
     }, false);
   }
 }
