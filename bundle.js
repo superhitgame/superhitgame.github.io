a99b281e754da2f6047cf727e422e49b4c4eccef
robrecht <robrechth@gmail.com>
2016-01-29 20:34:14 +0100

    fixed mouse out

--- bundle.js
+++ bundle.js
@@ -580,43 +580,52 @@
 	module.exports = InputArea;
 
 	function InputArea(canvas, drawingManager) {
-	  var self = this;
-	  self.canvas = canvas;
-	  self.drawingManager = drawingManager;
-	  if ("ontouchstart" in window) {
-	    //log("init touch"); 
-	    self.canvas.addEventListener("touchstart", function(e) {
-	      e.preventDefault();
-	      var outer = self.canvas.getBoundingClientRect();
-	      self.drawingManager.startPen(e.touches[0].pageX - outer.left, e.touches[0].pageY - outer.top);
-	    }, false);
-	    self.canvas.addEventListener("touchend", function(e) {
-	      e.preventDefault();
-	      self.drawingManager.stopPen();
-	    }, false);
-	    self.canvas.addEventListener("touchmove", function(e) {
-	      e.preventDefault();
-	      var outer = self.canvas.getBoundingClientRect();
-	      self.drawingManager.movePen(e.touches[0].pageX - outer.left, e.touches[0].pageY - outer.top);
-	    }, false);
-	  } else {
-	    //log("init mouse");
-	    self.canvas.addEventListener("mousedown", function(e) {
-	      e.preventDefault();
-	      var outer = self.canvas.getBoundingClientRect();
-	      self.drawingManager.startPen(e.pageX - outer.left, e.pageY - outer.top);
-	    }, false);
-	    self.canvas.addEventListener("mouseup", function(e) {
-	      e.preventDefault();
-	      var outer = self.canvas.getBoundingClientRect();
-	      self.drawingManager.stopPen(e.pageX - outer.left, e.pageY - outer.top);
-	    }, false);
-	    self.canvas.addEventListener("mousemove", function(e) {
-	      e.preventDefault();
-	      var outer = self.canvas.getBoundingClientRect();
-	      self.drawingManager.movePen(e.pageX - outer.left, e.pageY - outer.top);
-	    }, false);
-	  }
+	    var self = this;
+	    self.canvas = canvas;
+	    self.drawingManager = drawingManager;
+	    self.mouseDown = false;
+	    if ("ontouchstart" in window) {
+	        //log("init touch"); 
+	        self.canvas.addEventListener("touchstart", function(e) {
+	            e.preventDefault();
+	            var outer = self.canvas.getBoundingClientRect();
+	            self.drawingManager.startPen(e.touches[0].pageX - outer.left, e.touches[0].pageY - outer.top);
+	        }, false);
+	        self.canvas.addEventListener("touchend", function(e) {
+	            e.preventDefault();
+	            self.drawingManager.stopPen();
+	        }, false);
+	        self.canvas.addEventListener("touchmove", function(e) {
+	            e.preventDefault();
+	            var outer = self.canvas.getBoundingClientRect();
+	            self.drawingManager.movePen(e.touches[0].pageX - outer.left, e.touches[0].pageY - outer.top);
+	        }, false);
+	    } else {
+	        //log("init mouse");
+	        self.canvas.addEventListener("mousedown", function(e) {
+	            var outer = self.canvas.getBoundingClientRect();
+	            self.drawingManager.startPen(e.pageX - outer.left, e.pageY - outer.top);
+	            self.mouseDown = true;
+	        });
+	        document.addEventListener("mousemove", function(e) {
+	            e.preventDefault();
+	            var outer = self.canvas.getBoundingClientRect();
+	            self.drawingManager.movePen(e.pageX - outer.left, e.pageY - outer.top);
+	        });
+	        document.documentElement.addEventListener('mouseup', function(e){
+	            var outer = self.canvas.getBoundingClientRect();
+	            self.drawingManager.stopPen(e.pageX - outer.left, e.pageY - outer.top);
+	        }); 
+	        document.documentElement.addEventListener('mouseover', function(e){
+	            if (e.target == this && e.relatedTarget == null) {
+	                if (mouseDown && !e.button) {
+	                    //Mouse up (entering document without pressing button));
+	                    mouseDown = false;
+	                    self.drawingManager.stopPen();
+	                }
+	            }
+	        });
+	    }
 	}
 
 
